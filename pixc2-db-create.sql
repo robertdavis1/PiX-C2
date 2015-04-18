c99677c90374053fb08e9393cd98253eed643f0c
Robert Davis <robert.ant.davis@gmail.com>
2015-04-18 14:29:00 -0400

    Update and rename pingc2-db-create.sql to pixc2-db-create.sql

--- pixc2-db-create.sql
+++ pixc2-db-create.sql
@@ -1,4 +1,4 @@
-create database pingc2;
-use pingc2;
+create database pixc2;
+use pixc2;
 create table bots (id int primary key not null  auto_increment, remoteip varchar(16), name varchar(100), os varchar(25), localip varchar(16), checkin date);
 
